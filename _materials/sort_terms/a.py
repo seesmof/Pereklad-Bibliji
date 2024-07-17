59b3e26d43edeb354e6ea1979551e558b0586cf2
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-07-17 20:00:46 +0300

    feat: Hallelujah thank YOU JESUS CHRIST our HOLY LORD GOD ALMIGHTY KING MOST HIGH ✝️💓

--- _materials/sort_terms/a.py
+++ _materials/sort_terms/a.py
@@ -17,19 +17,7 @@ with open(terms_file_path, "r", encoding="utf-8") as file:
         Bible_terms.append(line.strip())
 
 Bible_terms = sorted(Bible_terms)
-for term in Bible_terms:
-    original, translation = term.split(":", 1)
-    original, translation = original.strip(), translation.strip()
 
-    translation = safe_split(translation, ";")
-    translation = [
-        translation.strip()
-        for translation in translation
-        if translation != ";" and translation != ""
-    ]
-    affirmed: str = translation[0]
-    options: list[str] | None = translation[1:]
-    if options:
-        options = options[0].split(",")
-        options = [option.strip() for option in options if option != ""]
-    print(f"{original}:{affirmed};{','.join(options) if options else ''}")
+with open(terms_file_path, "w", encoding="utf-8") as file:
+    for term in Bible_terms:
+        file.write(f"{term}\n")
