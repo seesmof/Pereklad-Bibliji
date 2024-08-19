20721632a7c847bf216374c36c6b917d95a38349
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-08-19 11:09:02 +0300

    feat: MAT title

--- SUB Standard Ukrainian Bible/translating.md
+++ SUB Standard Ukrainian Bible/translating.md
@@ -1,3 +1,4 @@
+start translating with NT, then move to OT
 1. read through the section: source, T4T, check
 2. translate the section
 3. backwards-translate the section
--- actions/copy_files/run.py
+++ actions/copy_files/run.py
@@ -4,14 +4,14 @@ from shutil import copy2 as copy_file
 
 current_dir: str = os.path.dirname(os.path.abspath(__file__))
 Bible_Books_target_dir: str = os.path.join(
-    current_dir, "..", "..", "Ukrainian Standard Bible"
+    current_dir, "..", "..", "SUB Standard Ukrainian Bible"
 )
-Bible_project_dir: str = os.path.join("C:\\My Paratext 9 Projects\\USB")
+Bible_project_dir: str = os.path.join("C:\\My Paratext 9 Projects\\SUB")
 
 for file in os.listdir(Bible_Books_target_dir):
     file_name, file_extension = os.path.splitext(file)
     if file_extension == ".usfm":
         copy_file(
             os.path.join(Bible_Books_target_dir, file),
-            os.path.join(Bible_project_dir, file_name + ".SFM"),
+            os.path.join(Bible_project_dir, file_name + ".USFM"),
         )
